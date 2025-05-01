c9f7a64b23d213399fed90cdd8d7d0b53dc36fbf
Daniel Newton <djpnewton@gmail.com>
2025-05-01 09:20:44 -0400

    move chart info into a custom widget using stack

--- lib/vis2.dart
+++ lib/vis2.dart
@@ -47,10 +47,10 @@ class _VisState extends State<VisFinancialChart> with TickerProviderStateMixin {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final theme = GThemeLight();
-    final dataSource = _getDataSource();
+  Widget _buildInfoCard(BuildContext context) {
+    if (widget.filteredData.isEmpty) {
+      return const SizedBox.shrink();
+    }
     final startValue = widget.filteredData.first.amount;
     final endValue = widget.filteredData.last.amount;
     final change = endValue - startValue;
@@ -63,67 +63,101 @@ class _VisState extends State<VisFinancialChart> with TickerProviderStateMixin {
       final dateStr = formatter.format(firstDate);
       timeFrame = 'Since $dateStr';
     }
-    return GChartWidget(
-      chart: GChart(
-        dataSource: dataSource,
-        theme: theme,
-        // disable scaling by mouse wheel
-        pointerScrollMode: GPointerScrollMode.none,
-        // set default point range
-        pointViewPort: GPointViewPort(
-          initialStartPoint: dataSource.firstPoint - 1,
-          initialEndPoint: dataSource.lastPoint + 1,
-          autoScaleStrategy: null,
-        ),
-
-        panels: [
-          GPanel(
-            valueViewPorts: [
-              GValueViewPort(
-                valuePrecision: 2,
-                autoScaleStrategy: GValueViewPortAutoScaleStrategyMinMax(
-                  dataKeys: [valueKey],
+    return Positioned(
+      top: 2,
+      left: 2,
+      child: Card(
+        // ignore: deprecated_member_use
+        color: Colors.white.withOpacity(0.8),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '${widget.dataSeries?.description}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
               ),
-            ],
-            valueAxes: [
-              GValueAxis(
-                axisMarkers: [
-                  GValueAxisMarker.label(
-                    labelValue:
-                        dataSource.getSeriesValue(
-                          point: dataSource.lastPoint,
-                          key: valueKey,
-                        )!,
-                  ),
-                ],
-              ),
-            ],
-            pointAxes: [
-              GPointAxis(
-                axisMarkers: [
-                  GPointAxisMarker.label(point: dataSource.lastPoint),
-                ],
-              ),
-            ],
-            graphs: [
-              GGraphGrids(),
-              GGraphLine(
-                valueKey: valueKey,
-                overlayMarkers: [
-                  GLabelMarker(
-                    text:
-                        '${widget.dataSeries?.description}\n$timeFrame  $changePercentString%',
-                    anchorCoord: GPositionCoord.absolute(x: 2, y: 2),
-                    alignment: Alignment.bottomRight,
+              Text('Time Frame: $timeFrame'),
+              Row(
+                children: [
+                  Text('Change: '),
+                  Text(
+                    '${change.toStringAsFixed(2)} ($changePercentString%)',
+                    style: TextStyle(
+                      color: change >= 0 ? Colors.green : Colors.red,
+                    ),
                   ),
                 ],
               ),
             ],
           ),
-        ],
+        ),
       ),
-      tickerProvider: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = GThemeLight();
+    final dataSource = _getDataSource();
+    return Stack(
+      children: [
+        GChartWidget(
+          chart: GChart(
+            dataSource: dataSource,
+            theme: theme,
+            // disable scaling by mouse wheel
+            pointerScrollMode: GPointerScrollMode.none,
+            // set default point range
+            pointViewPort: GPointViewPort(
+              initialStartPoint: dataSource.firstPoint - 1,
+              initialEndPoint: dataSource.lastPoint + 1,
+              autoScaleStrategy: null,
+            ),
+
+            panels: [
+              GPanel(
+                valueViewPorts: [
+                  GValueViewPort(
+                    valuePrecision: 2,
+                    autoScaleStrategy: GValueViewPortAutoScaleStrategyMinMax(
+                      dataKeys: [valueKey],
+                    ),
+                  ),
+                ],
+                valueAxes: [
+                  GValueAxis(
+                    axisMarkers: [
+                      GValueAxisMarker.label(
+                        labelValue:
+                            dataSource.getSeriesValue(
+                              point: dataSource.lastPoint,
+                              key: valueKey,
+                            ) ??
+                            0,
+                      ),
+                    ],
+                  ),
+                ],
+                pointAxes: [
+                  GPointAxis(
+                    axisMarkers: [
+                      GPointAxisMarker.label(point: dataSource.lastPoint),
+                    ],
+                  ),
+                ],
+                graphs: [GGraphGrids(), GGraphLine(valueKey: valueKey)],
+              ),
+            ],
+          ),
+          tickerProvider: this,
+        ),
+        _buildInfoCard(context),
+      ],
     );
   }
 }

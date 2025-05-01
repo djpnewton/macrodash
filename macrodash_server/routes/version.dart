12318602f42a8fceb4b69cd0be25f3a0171a84d0
Daniel Newton <djpnewton@gmail.com>
2025-05-01 12:11:03 -0400

    bump versions

--- macrodash_server/routes/version.dart
+++ macrodash_server/routes/version.dart
@@ -2,8 +2,8 @@ import 'package:dart_frog/dart_frog.dart';
 
 import 'package:macrodash_models/models.dart';
 
-const version = 7;
-const minClientVersion = 7;
+const version = 8;
+const minClientVersion = 8;
 
 Response onRequest(RequestContext context) {
   return Response.json(
